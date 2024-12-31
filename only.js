0a51c3bfdd61607ff2e38e2cd4f0ad470bd891b2
mofamsazfcdzomgames <mofafootball@ya.ru>
2024-12-31 12:46:10 +0400

    Update only.js

--- only.js
+++ only.js
@@ -7,9 +7,9 @@ function validatePromoCode() {
             const correctCode3 = 'BYE2024'
 
                 if (userCode === correctCode1) {
-                window.location.href = "promo2active.html"; 
+                window.location.href = "promo1active.html"; 
             } else if (userCode === correctCode2) {
-              window.location.href = "promo2active.html"
+              window.location.href = "promo1active.html"
             } else if (userCode === sponsorCode || userCode === correctCode3){
               window.location.href = "promo2active.html"
             } else {
